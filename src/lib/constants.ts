c4a262a69bebba4c29d9fd7615f565667e45fe4c
smastrom <smastrom@proton.me>
2025-04-24 21:51:10 +0200

    finalize layout, add index page content and date logic

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,13 @@
 export const RECIPIENT_NAME = 'Francesco'
+export const SENDER_NAME = '@smastrom'
+export const SENDER_URL = 'https://github.com/smastrom'
 
 export const SITE_URL_DEV = 'http://localhost:4321'
 export const SITE_URL = 'https://francyday.smastrom.io'
 export const SITE_NAME = 'francyday'
-export const SITE_DESCRIPTION = `Un regalo di compleanno da parte di @smastrom per ${RECIPIENT_NAME}.`
+export const SITE_DESCRIPTION = `Un regalo di compleanno da parte di ${SENDER_NAME} per ${RECIPIENT_NAME}.`
+
+export const THEME_COLOR = '#6C37E2'
 
 export const TYPEFACE = 'noticia-text-v15-latin'
 export const TYPEFACE_WEIGHTS = ['regular', 'italic', '700', '700italic']
-
-export const THEME_COLOR = '#6C37E2'
--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,3 +1,5 @@
+import { toZonedTime } from 'date-fns-tz'
+
 export const WEEK_DAYS = [
    'Domenica',
    'Lunedì',
@@ -23,14 +25,58 @@ export const MONTHS = [
    'Dicembre',
 ]
 
+export const TimeFrameCaptions = {
+   DAY: 'Buongiorno',
+   AFTERNOON: 'Buon pomeriggio',
+   NIGHT: 'Buonasera',
+}
+
+export function getZonedDate(date = new Date()) {
+   return toZonedTime(date, 'Europe/Rome')
+}
+
 export function getDayName(date = new Date()) {
+   date = getZonedDate(date)
    return WEEK_DAYS[date.getDay()]
 }
 
 export function getMonthName(date = new Date()) {
+   date = getZonedDate(date)
    return MONTHS[date.getMonth()]
 }
 
 export function getItalianDate(date = new Date()) {
-   return `${getDayName(date)} ${date.getDate()} ${getMonthName(date)} ${date.getFullYear()}`
+   date = getZonedDate(date)
+   return `${date.getDate()} ${getMonthName(date)} ${date.getFullYear()}`
+}
+
+export function getItalianDateWithDay(date = new Date()) {
+   date = getZonedDate(date)
+   return `${getDayName(date)}, ${getItalianDate(date)}`
+}
+
+export function getTimeUntilNextDay(date = new Date()) {
+   date = getZonedDate(date)
+
+   const nextDay = new Date(date)
+   nextDay.setDate(nextDay.getDate() + 1)
+   nextDay.setHours(0, 0, 0, 0)
+
+   const timeUntilNextDay = nextDay.getTime() - date.getTime()
+
+   return {
+      h: Math.floor(timeUntilNextDay / (1000 * 60 * 60)),
+      m: Math.floor((timeUntilNextDay % (1000 * 60 * 60)) / (1000 * 60)),
+      s: Math.floor((timeUntilNextDay % (1000 * 60)) / 1000),
+   }
+}
+
+export function getTimeFrameCaption(date = new Date()) {
+   date = getZonedDate(date)
+
+   return date.getHours() < 12
+      ? TimeFrameCaptions.DAY
+      : date.getHours() < 18
+        ? TimeFrameCaptions.AFTERNOON
+        : TimeFrameCaptions.NIGHT
 }
